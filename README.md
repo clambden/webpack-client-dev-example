307d7251825ec39540c3850182164138fa562b88
SCOTT\clambden <clambden@scottlogic.co.uk>
2018-10-15 12:11:41 +0100

    Created template app with basic webpack, babel and react config

---
+++ README.md
@@ -0,0 +1,3 @@
+# webpack-client-dev-example
+
+Sample application that demonstrates how webpack can be used to streamline the development process
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: "development",
+    entry: [
+        "./src/index.js"
+    ],    
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'My App',
+            template: 'public/index.html',
+            favicon: "public/favicon.png"
+        })
+    ],        
+    output: {
+        path: path.resolve(__dirname, "./dist"),
+        filename: "bundle.js",
+        publicPath: "/"
+    },
+    module: {
+        rules: [
+          {
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env', '@babel/preset-react']
+              }
+            }          },
+        ]
+    }    
+};
